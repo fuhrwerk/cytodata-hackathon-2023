f1fc4d2dde465f93755239fd2cdad4edbc50c571
Laxman <48151266+yetulaxman@users.noreply.github.com>
2023-10-06 15:59:33 +0300

    Update install_Cytohackathon.sh

--- install_Cytohackathon.sh
+++ install_Cytohackathon.sh
@@ -1,7 +1,7 @@
 module load git
 module load purge
 module load tykky
-mkdir /projappl/project_xxxx/$USER
+mkdir -p /projappl/project_xxxx/$USER
 mkdir -p /projappl/project_xxxx/$USER/CytoHackathon
 conda-containerize new --prefix  /projappl/project_xxxx/$USER/CytoHackathon Cytohackathon_py310.yml
 export PATH="/projappl/project_xxxx/$USER/CytoHackathon/bin:$PATH"
