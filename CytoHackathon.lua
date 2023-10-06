412c1fcd5299bd41b2502748561594218aad39a0
Laxmana Yetukuri <yetukuri@puhti-login14.bullx>
2023-10-06 13:46:06 +0300

    add compute scripts

---
+++ CytoHackathon.lua
@@ -0,0 +1,14 @@
+-- Jupyter
+prepend_path("PATH", "/projappl/project_2001659/yetukuri/CytoHackathon/bin")
+depends_on("gcc/9.4.0","cudnn/8.1.0.77-11.2","cuda/11.2.2")
+prepend_path('PATH',"/appl/bin:/appl/spack/v018/install-tree/gcc-9.4.0/cuda-10.1.243-c7m3zk/bin")
+
+
+setenv("_COURSE_BASE_NAME", "CytoHackathon")
+setenv("_COURSE_NOTEBOOK", "CytoHackathon/notebook/CytoData2023_hackathon.ipynb")
+setenv("_COURSE_GIT_REPO", "https://github.com/yetulaxman/CytoHackathon.git")
+setenv("_COURSE_GIT_REF", "")
+setenv("_COURSE_NOTEBOOK_TYPE", "notebook")
+
+-- Extra course material
+setenv("_COURSE_SRC", "")
