a4b2a9c2408cacd71988f22ed101f72fb429be58
Laxman <48151266+yetulaxman@users.noreply.github.com>
2023-10-06 14:30:43 +0300

    Update install_Cytohackathon.sh

--- install_Cytohackathon.sh
+++ install_Cytohackathon.sh
@@ -1,11 +1,8 @@
-# remove all installations from home: rm -fr ~/.local/*; 
-#export TMPDIR="/scratch/project_2001659/yetukuri/CytoHackathon/install_setup/tmp"
-#export LOCAL_HOST="/scratch/project_2001659/yetukuri/CytoHackathon/install_setup/tmp"
 module load git
 module load purge
 module load tykky
-mkdir /projappl/project_2001659/yetukuri
-mkdir /projappl/project_2001659/yetukuri/CytoHackathon
-conda-containerize new --prefix  /projappl/project_2001659/yetukuri/CytoHackathon Cytohackathon_py310.yml
-export PATH="/projappl/project_2001659/yetukuri/CytoHackathon/bin:$PATH"
-conda-containerize update /projappl/project_2001659/yetukuri/CytoHackathon/  --post-install requirements_Cytohackathon.sh
+mkdir /projappl/project_xxxx/$USER
+mkdir -p /projappl/project_xxxx/$USER/CytoHackathon
+conda-containerize new --prefix  /projappl/project_xxxx/$USER/CytoHackathon Cytohackathon_py310.yml
+export PATH="/projappl/project_xxxx/$USER/CytoHackathon/bin:$PATH"
+conda-containerize update /projappl/project_xxxx/$USER/CytoHackathon/  --post-install requirements_Cytohackathon.sh
